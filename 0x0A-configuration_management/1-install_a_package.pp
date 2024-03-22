19b511d75097139efb15859842847436e547f519
tarik khadri <tarikkhadri11@gmail.com>
2024-03-22 13:25:09 -0700

    fsa

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Puppet manifest to install Flask using pip3
+
+package { 'flask':
+  ensure   => '2.1.0',  # Specify the version of Flask to install
+  provider => 'pip3',   # Use pip3 as the provider
+  require  => Package['python3-pip'], # Ensure pip3 is installed first
 }
+
