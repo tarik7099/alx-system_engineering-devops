4c3a4d2925a6cdcf1ee76ef3b76ecb61bba60c91
tarik khadri <tarikkhadri11@gmail.com>
2024-03-22 16:52:02 -0700

    f

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#install a flask from pip3
-exec { 'install flask':
-  command =>  'pip3 install flask==2.1.0',
-  path    =>  '/usr/bin/',
-  unless  =>  'pip3 list | grep flask',
+#Install flask version 2.1.0
+
+
+exec {'pip3 install flask':
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
+}
+
+exec {'python-installed':
+command => '/usr/bin/which python3'
 }
