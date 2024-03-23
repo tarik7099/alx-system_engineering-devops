839049a601202961bcce5a131a6e9902c1817cee
root <tarikkhadri11@gmail.com>
2024-03-22 19:42:49 -0700

    df

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
-#Install flask version 2.1.0
-
-
-exec {'pip3 install flask':
-require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
-exec {'python-installed':
-command => '/usr/bin/which python3'
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
+
