c4aebd3ba6a4cd5e7f3f313538d28684e07cd5ad
tarik khadri <tarikkhadri11@gmail.com>
2024-05-07 23:33:51 +0100

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and returns the number
-of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given,
-the function should return 0.
+number of subscribers for a given subreddit
 """
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns number of total subscribers"""
-    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Returns the number of subscribers for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit.
+        
+    Returns:
+        int: The number of subscribers for the subreddit. Returns 0 if the subreddit is invalid or if the request fails.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
 
-    if response.status_code != 200:
-        return (0)
-    response = response.json()
-    if 'data' in response:
-        return (response.get('data').get('subscribers'))
-
-    else:
-        return (0)
+    user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    try:
+        response = requests.get(url, headers=user_agent)
+        response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return 0
+    except (KeyError, ValueError):
+        return 0
