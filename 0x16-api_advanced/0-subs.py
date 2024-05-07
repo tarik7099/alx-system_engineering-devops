143abaaa51a68f95c23d5e75131541057d3b0e75
tarik khadri <tarikkhadri11@gmail.com>
2024-05-07 22:57:28 +0100

    tasl 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+""" Queries the Reddit API and returns the number of subscribers """
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+def number_of_subscribers(subreddit) -> int:
+    """
+    Queries the Reddit API and returns the number of subscribers
+
+    Args:
+        subreddit (str): subreddit to query
+
+    Returns:
+        (int): number of subscribers
+    """
+    # Returns the number of subscribers for a given subreddit
+    base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    try:
+        request = requests.get(base_url, headers={'User-Agent': 'Agent Uche'})
+        data = request.json()
+        return data['data']['subscribers']
+    except Exception:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
